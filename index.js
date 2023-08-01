75816c536cc5f37455ac4eb5857e30d78736846d
Mr. E <mr.e@Katrinas-iMac.local>
2023-07-31 22:48:52 -0400

    added questions according to readme

--- index.js
+++ index.js
@@ -4,32 +4,54 @@ const fs = require('fs');
 const question = [
         {
             type: "input",
-            name: "firstName",
-            message: "What is your first name?"
+            name: "title",
+            message: "What is the title of your project?"
         },
         {
             type: "input",
-            name: "lastName",
-            message: "What is your last name?"
+            name: "description",
+            message: "Enter a description?"
         },
         {
-            type: "list",
-            name: "color",
-            message: "What is your favourite color?",
-            choices: ["black", "brown", "yellow", "blue", "green"]
+            type: "input",
+            name: "installation",
+            message: "Enter installation instructions:"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Enter usage information:",
+        },
+        {
+
+            type: "input",
+            name: "contributing",
+            message: "Enter contribution guidelines:"
+        },
+        {
+
+            type: "input",
+            name: "tests",
+            message: "Enter test instructions"
         }
-    ]
+    ];
 inquirer.prompt(question)   
 .then(function(answer){
     console.log(answer);
 
     const content = `#Answers
-    ## First name
-   - ${answer.firstName}
-    ## Last name
-   - ${answer.lastName}
-    ## Language
-   - ${answer.color}
+    ## title
+   - ${answer.title}
+    ## description 
+   - ${answer.description}
+    ## installation
+   - ${answer.installation}
+   ## usage
+   - ${answer.usage}
+    ## contributing 
+   - ${answer.contributing}
+    ## tests
+   - ${answer.tests}
     `
     
     fs.writeFile('assets/README.md', content, (error) => {
